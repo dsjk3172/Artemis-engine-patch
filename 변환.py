d345ef210e0b8d5275546fb9f4d412e589d55eca
이시현 <dsjk3172@naver.com>
2022-11-22 11:45:25 +0900

    11/22 update

--- 변환.py
+++ 변환.py
@@ -7,14 +7,12 @@ fileiist = os.listdir(f"{path}/원본")
 for i in fileiist:
     ReadFile = open(f"{path}/원본/{i}", 'r' , encoding='UTF-8')
     ReadFile2 = open(f"{path}/번역본/{i}", 'r' , encoding='UTF-16')
-    WriteFile = open(f"{path}/최종결과물/{i}", 'w', encoding='UTF-8')
+    WriteFile = open(f"{path}/1차 결과물/{i}", 'w', encoding='UTF-8')
     lines = ReadFile.readlines()
 
+    r = re.compile('.*[ぁ-ゔァ-ヴー々〆〤].*$')
     for j in lines:
         aaa = ReadFile2.readline().strip("\n")
-        #aaa = ReadFile2.readline()
-        if(aaa == ''):
-            WriteFile.write(j)
-        else:
-            WriteFile.write(aaa)
+        contents = re.sub(r, aaa, j)
+        WriteFile.write(contents)
 WriteFile.close()
--- 추출.py
+++ 추출.py
@@ -6,15 +6,14 @@ filelist = os.listdir(f"{path}/원본")
 
 for i in filelist:
     ReadFile = open(f"{path}/원본/{i}", 'r' , encoding='UTF-8')
-    writefile = open(f"{path}/추출본/{i}", 'w', encoding='UTF-8')
+    writefile = open(f"{path}/일본어 추출본/{i}", 'w', encoding='UTF-8')
     readline = ReadFile.readlines()
 
     abc = list()
 
-    r = re.compile('.*[a-z].*$')
+    r = re.compile('.*[ぁ-ゔァ-ヴー々〆〤].*$')
     for i in readline:
-        #abc = re.findall(r, i)
-        abc = re.sub(r, '', i)
+        abc = re.findall(r, i)
         contents = "".join(abc) + "\n"
         writefile.write(contents)
     writefile.close()
