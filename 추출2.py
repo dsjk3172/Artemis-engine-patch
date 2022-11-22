932603b166a7799c91cb8c6000c610bab0b3e73f
이시현 <dsjk3172@naver.com>
2022-11-22 12:47:20 +0900

    11/22 update

---
+++ 추출2.py
@@ -0,0 +1,19 @@
+import re
+import os.path
+
+path = os.getcwd()
+filelist = os.listdir(f"{path}/원본")
+
+for i in filelist:
+    ReadFile = open(f"{path}/원본/{i}", 'r' , encoding='UTF-8')
+    writefile = open(f"{path}/시스템 문장 추출본/{i}", 'w', encoding='UTF-8')
+    readline = ReadFile.readlines()
+
+    abc = list()
+
+    r = re.compile('.*[a-zA-Z].*$')
+    for i in readline:
+        abc = re.findall(r, i)
+        contents = "".join(abc) + "\n"
+        writefile.write(contents)
+    writefile.close()
